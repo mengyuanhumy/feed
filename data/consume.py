aec16c1edbd377ffb9a16ef8603dbc87e4090943
Y740s <Y740s@LAPTOP-4L1CIE65>
2020-04-23 23:59:07 +0800

    完成2个填报表单，和sql计算函数

--- data/consume.py
+++ data/consume.py
@@ -2,11 +2,43 @@
  
 from django.shortcuts import render
 from django.views.decorators import csrf
+from mysql.sqlConnect import *
  
+def insert(res):
+    """
+    """
+    #初始化
+    query="insert into consume (item_name,date,quantity) values (%s,%s,%s)"
+    param=[]
+    result={"param":"","row":0}
+    row=0
+    #从网页获得数据
+    if res['bg']: param.append(('bg',res['p_date'],res['bg']))
+    if res['gn']: param.append(('gn',res['p_date'],res['gn']))
+    if res['hm']: param.append(('hm',res['p_date'],res['hm']))
+    if res['hty']: param.append(('hty',res['p_date'],res['hty']))
+    if res['mt']: param.append(('mt',res['p_date'],res['mt']))
+    if res['nf']: param.append(('nf',res['p_date'],res['nf']))
+    if res['pf']: param.append(('pf',res['p_date'],res['pf']))
+    if res['day']: param.append(('day',res['p_date'],res['day']))
+    if res['sgm']: param.append(('sgm',res['p_date'],res['sgm']))
+    if res['night']: param.append(('night',res['p_date'],res['night']))
+    if res['ydl']: param.append(('ydl',res['p_date'],res['ydl']))
+    if res['mf']: param.append(('mf',res['p_date'],res['mf']))
+            #print(param)
+    #增加到数据库
+    if len(param)>0:
+        sqlConnection()
+        row=sqlInsert(query,param)
+        sqlDisconnection()
+              
+    #记录结果        
+    result={"param":param,"row":row}
+    return result
+
 # 接收POST请求数据
 def consume(request):
-    ctx ={}
+    ctx={}
     if request.POST:
-        ctx['rlt'] = request.POST['milk']
-        ctx['r2'] = request.POST['wiper']
+        ctx=insert(request.POST)
     return render(request, "consume_form.html", ctx)
--- data/purchase.py
+++ data/purchase.py
@@ -14,18 +14,18 @@ def insert(res):
     result={"param":"","row":0}
     row=0
     #从网页获得数据
-    if res['bg']: param.append(('bg',res['p_date'],res['bg'],'0',res['store']))
-    if res['gn']: param.append(('gn',res['p_date'],res['gn'],'0',res['store']))
-    if res['hm']: param.append(('hm',res['p_date'],res['hm'],'0',res['store']))
-    if res['hty']: param.append(('hty',res['p_date'],res['hty'],'0',res['store']))
-    if res['mt']: param.append(('mt',res['p_date'],res['mt'],'0',res['store']))
-    if res['nf']: param.append(('nf',res['p_date'],res['nf'],'0',res['store']))
-    if res['pf']: param.append(('pf',res['p_date'],res['pf'],'0',res['store']))
-    if res['day']: param.append(('day',res['p_date'],res['day'],'0',res['store']))
-    if res['sgm']: param.append(('sgm',res['p_date'],res['sgm'],'0',res['store']))
-    if res['night']: param.append(('night',res['p_date'],res['night'],'0',res['store']))
-    if res['ydl']: param.append(('ydl',res['p_date'],res['ydl'],'0',res['store']))
-    if res['mf']: param.append(('mf',res['p_date'],res['mf'],'0',res['store']))
+    if res['bg']: param.append(('bg',res['p_date'],res['bg'],res['bg_p'],res['store']))
+    if res['gn']: param.append(('gn',res['p_date'],res['gn'],res['gn_p'],res['store']))
+    if res['hm']: param.append(('hm',res['p_date'],res['hm'],res['hm_p'],res['store']))
+    if res['hty']: param.append(('hty',res['p_date'],res['hty'],res['hty_p'],res['store']))
+    if res['mt']: param.append(('mt',res['p_date'],res['mt'],res['mt_p'],res['store']))
+    if res['nf']: param.append(('nf',res['p_date'],res['nf'],res['nf_p'],res['store']))
+    if res['pf']: param.append(('pf',res['p_date'],res['pf'],res['pf_p'],res['store']))
+    if res['day']: param.append(('day',res['p_date'],res['day'],res['day_p'],res['store']))
+    if res['sgm']: param.append(('sgm',res['p_date'],res['sgm'],res['sgm_p'],res['store']))
+    if res['night']: param.append(('night',res['p_date'],res['night'],res['night_p'],res['store']))
+    if res['ydl']: param.append(('ydl',res['p_date'],res['ydl'],res['ydl_p'],res['store']))
+    if res['mf']: param.append(('mf',res['p_date'],res['mf'],res['mf_p'],res['store']))
             #print(param)
     #增加到数据库
     if len(param)>0:
