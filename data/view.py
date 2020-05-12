98bd1bf71dd773a263db1a96ecc3e11a9c4a5157
Y740s <Y740s@LAPTOP-4L1CIE65>
2020-05-12 22:32:48 +0800

    找到了Django写动态表的方法

--- data/view.py
+++ data/view.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
- 
+
 def home(request):
     context = {}
     context['welcome'] = '采购供给系统'
+    context['th']=[1,2,3]
+    context['td']=['a','b','c']
     return render(request, 'homepage.html', context)
